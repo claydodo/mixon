6676d33a9eb739378801acad5c68f740d01edd19
yangdw <claydodo@foxmail.com>
2021-10-19 18:02:19 +0800

    v0.0.1

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+python3 -m build
+
+VERSION=`cat setup.cfg | grep 'version = ' | grep -o '[0-9]*\.[0-9]*\.[0-9]*'`
+
+twine upload dist/*${version}*
---
+++ mixon/__init__.py
@@ -0,0 +1,14 @@
+__all__ = ["load", "dump", "loads", "dumps"]
+
+
+def load():
+    pass
+
+def dump():
+    pass
+
+def loads():
+    pass
+
+def dumps():
+    pass
